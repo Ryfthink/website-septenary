d2549d23278c37d2c952fb2e71ee34f75eaef081
Septenary <seven__up@sina.cn>
2018-02-03 15:07:26 +0800

    refactor: db connection 方式

--- server/db/dbconnection.ts
+++ server/db/dbconnection.ts
@@ -3,11 +3,25 @@ import * as mongoose from "mongoose";
 (<any>mongoose).Promise = global.Promise;
 
 function connectDB(onDBConnected: Function) {
-    const connection = mongoose.connect(DBConfig.host, {server: {socketOptions: {keepAlive: 1}}}).connection;
-    connection.connect(DBConfig.host, {server: {socketOptions: {keepAlive: 1}}})
-       .on('error', onError)
-       .on('disconnected', connectDB)
-       .once('open', onDBConnected);
+    // const connection = mongoose.connect(DBConfig.host, {server: {socketOptions: {keepAlive: 1}}}).connection;
+    // connection.connect(DBConfig.host, {server: {socketOptions: {keepAlive: 1}}})
+    //    .on('error', onError)
+    //    .on('disconnected', connectDB)
+    //    .once('open', onDBConnected);
+
+    let uri = DBConfig.host;
+    let options = {}
+
+ 	mongoose.connect(uri, options).then(
+	  () => { 
+	  	/** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
+	  	onDBConnected()
+	  },
+	  err => { 
+	  	/** handle initial connection error */ 
+	  	onError(err)
+	  }
+	);
 }
 
 function onError(error) {
